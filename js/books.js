5be7375b493b7952c1d9ed97613a4f43df3a7d2c
maser0 <morawski.andrzej@o2.pl>
2020-05-08 22:41:19 +0200

    warszatat JS piatek

--- js/books.js
+++ js/books.js
@@ -2,20 +2,24 @@ $(function() {
     // var dateContainer = $("#date");
     // // http://date.jsontest.com
         var div1 = $("#books");
+        getBooks();
+        function getBooks(){
         $.ajax({
             url: "http://localhost:8282/books/",
             type: "GET"
         }).done(function(result) {
+                div1.empty();
             var booksArray = result;
             for (var i = 0; i <booksArray.length ; i++) {
                     var input = booksArray[i].title;
                     var idBook = booksArray[i].id;
                     console.log(input);
-                    var element = $('<div class="book" data-id=' +idBook + '><h2>' + input + '</h2><div class="book-content"></div></div>');
+                    var element = $('<div class="book" data-id=' +idBook + '><h2>' + input + '</h2><div class="book-content"></div><a href="#" class="link", data-id=' +idBook + '>Link</a></div>');
                     div1.append(element);
             }
         });
-var buttons = $('.button')
+        }
+
 
 
 
@@ -37,20 +41,40 @@ var buttons = $('.button')
     var inputAutor = $('#autor');
     var inputIsbn = $('#isbn');
     var inputWydawca = $('#wydawca');
-    var inputAutorValue = inputAutor.value;
-    var inputIsbnValue = inputIsbn.value;
-    var inputWydawcaValue = inputWydawca.value;
+    var inputTytul = $('#tytul');
+
+
     var button = $('#send');
 
+
+
     button.on('click', function () {
+        var inputAutorValue = inputAutor.val();
+        var inputIsbnValue = inputIsbn.val();
+        var inputWydawcaValue = inputWydawca.val();
+        var inputTytulValue = inputTytul.val();
+        var book = { title: inputTytulValue, author: inputAutorValue, isbn: inputIsbnValue, publisher: inputWydawcaValue };
         $.ajax({
-            url: 'http://example.com/books/',
-            data: '{"isbn":"inputAutor", "title":"Thinking in Java"}',
+            url: 'http://localhost:8282/books/',
+            data: JSON.stringify(book),
+            dataType: "json",
             contentType: "application/json",
-            method: "PUT"
-        }).done(function () {
+            method: "POST"
+        }).done(function() {
             alert('PUT completed');
+            getBooks();
+
         });
+
+    })
+
+
+    $(document).on('click', ".link", function (event) {
+        $.ajax({
+            url: "http://localhost:8282/books/" + this.dataset.id,
+            method: "DELETE"
+        });
+    });
 
 
 
